1238cdf910ea1086b11073c819a138a18912034e
ashwinjoy-art <ashwinjoy530@gmail.com>
2024-02-15 11:38:55 +0530

    test

---
+++ C/tempCodeRunnerFile.c
@@ -0,0 +1,11 @@
+ the element: ");
+	for(i=0;i<limit;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	printf("Entered array elements are: ");
+	for(i=0;i<limit;i++)
+	{
+	printf("%d ",arr[i]);
+    }
+	return 0;
