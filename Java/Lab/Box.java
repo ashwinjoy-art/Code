1d44012bfb38448ad832bde6a03f83402016c9a8
ashwinjoy-art <ashwinjoy530@gmail.com>
2024-02-20 09:32:19 +0530

    Update

---
+++ Java/Lab/Box.java
@@ -0,0 +1,21 @@
+class Box
+{
+    public static void main(String[] args) 
+    {
+        BoxDemo b1=new BoxDemo();
+        b1.getData(5.6,3.1,7.2);
+        b1.volume();
+    }
+}
+class BoxDemo
+{
+    double height; double width; double depth;
+    void getData(double h,double w, double d)
+    {
+        height=h; width=w; depth=d;
+    }
+    void volume()
+    {
+        System.out.print("Volume ="+height*width*depth);
+    }
+}
