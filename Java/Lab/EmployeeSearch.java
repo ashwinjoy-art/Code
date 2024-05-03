7293031591f9c1df0d6b597c618a63087f9df29d
ashwinjoy-art <ashwinjoy530@gmail.com>
2024-05-03 18:22:52 +0530

    Java Update

--- Java/Lab/EmployeeSearch.java
+++ Java/Lab/EmployeeSearch.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 
-class Employee 
+class Employee1 
 {
     private int eNo;
     private String eName;
     private double eSalary;
 
-    public Employee(int eNo, String eName, double eSalary) 
+    public Employee1(int eNo, String eName, double eSalary) 
     {
         this.eNo = eNo;
         this.eName = eName;
@@ -36,7 +36,7 @@ public class EmployeeSearch
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the number of employees: ");
         int n = scanner.nextInt();
-        Employee[] employees = new Employee[n];
+        Employee1[] employees = new Employee1[n];
         for (int i = 0; i < n; i++) {
             System.out.println("Enter details for employee " + (i + 1) + ":");
             System.out.print("Employee Number: ");
@@ -46,7 +46,7 @@ public class EmployeeSearch
             String eName = scanner.nextLine();
             System.out.print("Employee Salary: ");
             double eSalary = scanner.nextDouble();
-            employees[i] = new Employee(eNo, eName, eSalary);
+            employees[i] = new Employee1(eNo, eName, eSalary);
         }
 
         System.out.print("Enter the employee number to search: ");
