1e43fd90f718039b58f6b9c4ac691c4f2ba2caf2
ashwinjoy-art <ashwinjoy530@gmail.com>
2024-08-19 11:17:57 +0530

    Pandas

---
+++ Data Science/Pandas Named Indexes.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+data = {
+  "calories": [420, 380, 390],
+  "duration": [50, 40, 45]
+}
+
+df = pd.DataFrame(data, index = ["day1", "day2", "day3"])
+print(df) 
+#refer to the named index:
+print(df.loc["day2"])
