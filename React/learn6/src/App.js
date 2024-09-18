f0939656268c287d7d5c8d68ae5713a88bace5c3
ashwinjoy-art <ashwinjoy530@gmail.com>
2024-09-18 22:09:08 +0530

    React Learning

--- React/learn6/src/App.js
+++ React/learn6/src/App.js
@@ -14,6 +14,7 @@ function App() {
             <Route path='/about' element={<About/>} />
             <Route path='/profile' element={<Profile data={state}/>} />
           </Routes>
+          {setState} {/* dummy */}
     </div>
   );
 }
